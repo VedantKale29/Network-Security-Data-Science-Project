6d62ed0d07377e03507ec04d131cc88b631a95a5
Vedant Kale <vedant.221883105@vcet.edu.in>
2026-01-31 14:55:14 +0530

    Data ingestion Completed

--- push_data.py
+++ push_data.py
@@ -50,8 +50,8 @@ class NetworkDataExtract():
         
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="Vedant"
-    Collection="NetworkData"
+    DATABASE = "Vedant"
+    Collection = "NetworkSecurityData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
     print(records)
